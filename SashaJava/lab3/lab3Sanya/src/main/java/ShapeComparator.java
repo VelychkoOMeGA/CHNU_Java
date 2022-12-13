588c7d6567db9c8c4b22c893d2b7d85f6e3e46f2
VelychkoOMeGA <119452654+VelychkoOMeGA@users.noreply.github.com>
2022-12-14 00:28:37 +0200

    Delete SashaJava/lab3/lab3Sanya directory

--- SashaJava/lab3/lab3Sanya/src/main/java/ShapeComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class ShapeComparator implements Comparator<Shape> {
-
-    @Override
-    public int compare(Shape o1, Shape o2) {
-        return o1.getType().compareTo(o2.getType());
-    }
-}
